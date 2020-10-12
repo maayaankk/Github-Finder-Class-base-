69097ee7968cbbd0ed4910a4d923f74dd82751ad
Mayank Chotaliya <67364983+maayaankk@users.noreply.github.com>
2020-10-12 18:20:51 +0530

    Add files via upload

---
+++ Users.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Useritem from './Useritem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
+
+function Users({ users, loading }) {
+    if (loading) {
+        return <Spinner />
+    } else {
+        return (
+            <div style={userStyle}>
+                {users.map(user => (
+                    <Useritem key={user.id} user={user} />
+                ))}
+            </div>
+        )
+    }
+
+}
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+}
+
+const userStyle = {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridGap: '1rem'
+}
+
+export default Users;
